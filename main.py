30804ebf8fa5ada42dd2a64257e0058f462e0458
kdrcnuzmcu <kdrcn.uzmcu@gmail.com>
2023-01-12 15:33:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def association_(df, alive):
     diseases = diseases.dropna()
     diseases = diseases.applymap(lambda x: True if x == 1 else False)
     supports = apriori(diseases, min_support = 0.0005, use_colnames = True)
-    results = association_rules(a, metric = "support", min_threshold = 0.0005)
+    results = association_rules(supports, metric = "support", min_threshold = 0.0005)
     return results
 
 deads = association_(df = df, alive = 0)
